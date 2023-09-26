b520bb3a4a8b945e863ddc65cbf2444d2646fc0c
Top1Profgrammer123 <huseynovjaba@gmail.com>
2023-09-26 20:34:15 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,41 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+
+def print_hi(name):
+    # Use a breakpoint in the code line below to debug your script.
+    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+
+
+# Press the green button in the gutter to run the script.
+# if __name__ == '__main__':
+print_hi('PyCharm')
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+# a=int(input())
+# y=
+# a=a*a
+# print(a)
+# # print(int("5"),type(int("abv"))) Incorrect because "abv" isn't an integer
+# print(int("5"),type(int("5")))
+# print(str(345665),type(str(345)))
+# print(bool(""))
+# print(bool("abv"))
+# print(bool(345))
+# print(bool(int("0")))   #0 is identified as a int and because 0 doesn't have a value it returns as false
+# print(bool(""))
+# print(bool("0"))    #0 is identified as a string and because string contains a value it returns true but if it doesn't it returns false
+# def area_of_triangle(x, y):
+#     result = x * y / 2
+#     return result
+#
+#
+# b = (int(input("Enter b:")))
+# a = (int(input("Enter a:")))
+# z = area_of_triangle(a, b)
+# print(int(z))
+print(int(float("4.6")))   #Because we said that python will first read float and then we will get 4.6 that has type float then int will convert float into a whole number which is a round down number
