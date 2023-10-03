62ce2fbf0f3836f1231001b70a83ad424d865a7b
Top1Profgrammer123 <huseynovjaba@gmail.com>
2023-10-03 19:44:18 +0400

    lesson 3

---
+++ Homework.py
@@ -0,0 +1,61 @@
+# def volume_of_ball (x):
+#     result = 3/4*3.14*x**3
+#
+#     if  result <10:
+#      return print("small ball")
+#     elif result>10:
+#         return 'Big ball'
+#     else:
+#         return ("ball equals to 10")
+#     return result
+# print(volume_of_ball(1))
+
+def numbers_operations(x,y,z):
+     if z=='+':
+          return x+y
+     elif z=='-':
+          return x-y
+     else:
+          return x*y
+
+# print(numbers_operations(2,3,'+'))#5
+# print(numbers_operations(2,3,'-'))#-1
+# print(numbers_operations(2,3,'*'))#6
+x=int(input("Enter your number:"))
+y=int(input("Enter your number:"))
+z=str(input())
+# print(xzy)
+print(numbers_operations(x,y,z))
+numbers_operations(x,y,z)
+
+
+# x="'Hello world'"
+# print (x)
+
+
+
+
+
+
+
+# def welcome(name, location):
+#     print("Hi", name, "welcome to", location)
+#
+# welcome('Erik', 'this tutorial')
+
+
+# def say_hi(name):
+#     print('Hi, ' + name)
+#
+# print("Let's greet the entire world")
+# say_hi('world')
+#
+#
+# def abc_school(name):
+#     print('Huseynov,  ' + name)
+#
+# print('The most hard working student is:')
+# abc_school('Jabrayil')
+
+
+
---
+++ Lesson 2.py
@@ -0,0 +1,44 @@
+# def sum_of_numbers (x, y, z):
+#     q = x + y + z
+#     print(q)
+#     return q #when q is not present in the code then the code will have nothing there due to it not being returned
+
+#print(sum_of_numbers(2, 4, 6))
+# result=sum_of_numbers(2,4,6)
+# print(result)
+
+# def arguments (x):
+#     #print(str(x))
+#     print(type( x ))  #The function type will define if the argument that we put is a str bool int or something else and return it
+#     return x
+
+#print(arguments("jaba"))
+
+y=int(input("Enter your number:"))
+x=int(input("Enter your number:"))
+if x>y:
+    print("more")
+
+elif x==y:
+    print("Equal")
+
+else:
+    print("less")
+
+
+# def define_type(x):
+#     result=""
+#     if type(x)==int:
+#         result=("Its an integer")
+#     elif type(x)==bool:
+#         result=("Its boolean")
+#
+#     elif type(x)==str:
+#         result=("Its string")
+#
+#     return result
+#
+#
+
+
+
